68d7c7883604c263dbd67248f80baeb991edb2f9
Bruno <brunopratesssp@hotmail.com>
2019-11-21 21:05:38 -0200

    Trabalho finalizado. Comentários adicionados ao código

--- subscriber.py
+++ subscriber.py
@@ -1,36 +1,111 @@
 import sys
 import time
-
+import matplotlib.pyplot as plt
 from random import randint
 import json
 import os
-
+import math
+from multiprocessing import Queue
 import zmq
 
-def nosub(nrosala):
+# Função para truncar um número
+def truncate(number, digits) -> float:
+    stepper = pow(10.0, digits)
+    return math.trunc(stepper * number) / stepper
+
+# Função executada por cada nó subscriber responsável por realizar a leitura de cada mensagem
+# referente ao seu tópico inscrito
+def nosub(nrosala,url):
     ctx = zmq.Context.instance()
-    porta = 5546+nrosala
     subscriber = ctx.socket(zmq.SUB)
-    url = "tcp://localhost:5558"
+    if url is None:
+        url = "tcp://localhost:5558"
     subscriber.connect(url)
-
-    subscription = b"%03d" % nrosala
+    # Indíce usado para inserir o log de temperaturas em um dicionário. 
+    indice=0
+    # Nome do tópico inscrito, sendo no caso apenas o número da sala
+    subscription = b"%03d" % nrosala;
     subscriber.setsockopt(zmq.SUBSCRIBE, subscription)
-
+    # Loop para ler sem parar as mensagens recebidas
     while True:
         topic, data = subscriber.recv_multipart()
+        data = data.decode()
         assert topic == subscription
-        print(data)
+        # Desempacota o JSON recebido
+        json_decodificado = json.loads(data)
+        # Cria um log para enviar ao processo pai cada mensagem recebida
+        log = {"Indice":indice,"Sala":nrosala,"TimeStamp": json_decodificado['timestamp'],"Temperatura":json_decodificado['temperatura']}
+        # Envia o log para uma fila de comunicação entre processos
+        q.put(log)
+        indice+=1
 
+
+q = Queue()
 def main(url=None):
+    indice = 0
+    # Dicionário para armazenar os logs de temperaturas de todas salas
+    temp = {}
     nrosala=1
-    for i in range(10):
+    # Cria uma fila para realizar troca de mensanges entre processos
+    # Cria os subscribers, cada um inscrito no tópico referente a uma sala 
+    for i in range(8):
         pid = os.fork()
         if pid == 0:
-            nosub(nrosala)
+            nosub(nrosala,url)
             os._exit(0)
         else:
             nrosala+=1
+    # Dorme por 11 segundos para ser possível gerar um gráfico com as últimas 10 leituras de temperatura
+    time.sleep(11)
+    while True:
+        # Lê da fila todos os logs e armazena no dicionário
+        while not q.empty():
+            log = q.get()
+            ind = log["Indice"]
+            sala = log["Sala"]
+            timestamp = log["TimeStamp"]
+            temperatura = log["Temperatura"]
+            log = {"Sala":sala,"TimeStamp": timestamp,"Temperatura":temperatura}
+            if not temp.get(ind):
+                temp[ind]=[]
+            temp[ind].append(log)
+            if ind>indice:
+                indice=ind
+
+        media={}
+        tempo = []
+        # Plota no gráfico a variação de temperatura nos últimos 10 segundos de todas as salas
+        for j in range(8):
+            valores = []
+            for i in range(10):
+                for log in temp[indice-10+i]:
+                    if int(log["Sala"]) == int(j+1):
+                        valores.append(float(log['Temperatura']))
+                        if j==0:
+                            tempo.append(log['TimeStamp'])
+            xn = range(10)
+            lbl = "Sala " + str(j+1)
+            media[j+1] = sum(valores)/len(valores)
+            plt.plot(xn,valores,label=lbl)
+            valores = []
+        plt.xticks(xn,tempo)
+        plt.title("Variação de temperatura nas salas")
+
+        plt.grid(True)
+
+        plt.xlabel("Horário")
+        plt.ylabel("Temperatura")
+        plt.legend()
+        plt.ion()
+        plt.show()
+        plt.draw()
+        plt.pause(0.001)
+        plt.clf()
+        for i in range(8):        
+            med = truncate(media[i+1],2)
+            print("Temperatura média nos últimos 10 segundos na sala "+str(i+1)+": "+str(med))
 
 if __name__ == '__main__':
     main(sys.argv[1] if len(sys.argv) > 1 else None)
+
+
--- temperatura.py
+++ temperatura.py
@@ -4,44 +4,60 @@ import random
 import math 
 import json
 import os
+import sys
+from datetime import datetime
 
+# Função para truncar um número
 def truncate(number, digits) -> float:
     stepper = pow(10.0, digits)
     return math.trunc(stepper * number) / stepper
 
-def nocoletor(nrosala):
+# Função que será executada por cada processo fork simulando a variação de temperatura em salas
+def nocoletor(nrosala,url):
     ctx = zmq.Context.instance()
     publisher = ctx.socket(zmq.PUB)
-    publisher.connect("tcp://127.0.0.1:5557")
+    if url:
+        publisher.connect(url)
+    else:
+        publisher.connect("tcp://localhost:5557")
     # Ensure subscriber connection has time to complete
     time.sleep(1)
-    # Temperatura inicial
-    temperatura= random.randint(200,300) /10
+    # Temperatura inicial randômica
+    temperatura = random.randint(200,300) /10
 
     while True:
-        # Simulação de variação da temperatura, sendo possível variar de -0.5 a 0.5º a cada segundo
-        rand = random.randint(-5,6)
-        rand= rand/20
-        temperatura+=rand
+        # Simulação de variação da temperatura, sendo possível variar de -0.3 a 0.3º a cada segundo
+        rand = random.randint(-3,3)
+        rand = rand/10
+        temperatura += rand
         temperatura = truncate(temperatura,1)
-        json_string = '{"temperatura":'+str(temperatura)+', "sala":'+str(nrosala)+'}'
-        json_decodificado = json.loads(json_string)
+        now = datetime.now()
+        now = str(now)
+        now=now.split(' ')
+        now2 = now[1].split('.')
+        now3 = now2[0]
+        # Empacota os dados em uma string JSON
+        json_string = '{ "temperatura":"'+str(temperatura)+'", "timestamp":"'+str(now3)+'"}'
+        # Envia o pacote para o broker, sendo o tópico o número da sala
         publisher.send_multipart([
                 b"%03d" % nrosala,
                 json_string.encode(),
             ])
         time.sleep(1)
 
-nrosala=1
-for i in range(10):
-    pid = os.fork()
-    if pid == 0:
-        random.seed()
-        nocoletor(nrosala)
-        os._exit(0)
-    else:
-        nrosala+=1
-os.wait()
+def main(url=None):
+    nrosala=1
+    # Cria 8 ṕublishers com o Fork
+    for i in range(8):
+        pid = os.fork()
+        if pid == 0:
+            random.seed()
+            nocoletor(nrosala,url)
+            os._exit(0)
+        else:
+            nrosala+=1
+    os.wait()
     
-    
+if __name__ == '__main__':
+    main(sys.argv[1] if len(sys.argv) > 1 else None)    
     
